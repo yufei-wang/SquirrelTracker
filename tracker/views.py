bdf4e3734a6a4316320c0968c996db47296662bc
Yufei Wang <yw3588@columbia.edu>
2020-10-16 04:16:04 +0000

    Define models

--- tracker/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404,redirect
-from .models import Squirrel
-from .forms import SquirrelForm
-
-# Create your views here.
