f27f775e9aeb9970364e0d329c08365e4e1968ba
Yufei Wang <yw3588@columbia.edu>
2020-10-19 05:38:59 +0000

    Update urls.py for add

--- sightings/urls.py
+++ sightings/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
         path('map/',views.map),
         path('sightings/', views.sightings,name='sightings'),
         path('<str:unique_squirrel_id>',views.squirrel_id,name='squirrel_id'),
-        path('add/', views.add, name='add'),
+        path('sightings/add/', views.add, name='add'),
         path('stats/', views.stats,name='stats'),        
         ]
